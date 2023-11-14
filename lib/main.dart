b1d7808c4b1d4d2d1c26b2f43b08a675d2965076
Juan Sillero <juan.sillero@musala.com>
2023-11-14 23:17:03 +0200

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:chat_app/routes/app_routes.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Chat demo app',
+      initialRoute: 'loading',
+      routes: appRoutes,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+    );
+  }
+}
